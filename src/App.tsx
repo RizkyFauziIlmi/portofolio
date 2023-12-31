552872e2a230d1be8c03f1f79ba7e6fc2f1416a3
RizkyFauziIlmi <rizkyfauziilmi@rizkyfauziilmi.com>
2023-12-31 19:37:48 +0700

    setup project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <h1 className="font-bold text-xl">Rizky Fauzi Ilmi portofolio</h1>
+    </div>
+  )
+}
+
+export default App
