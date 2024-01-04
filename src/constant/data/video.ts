5d80c2e1a64912ce40478ee18b6901d84ffc3ca6
RizkyFauziIlmi <rizkyfauziilmi@rizkyfauziilmi.com>
2024-01-04 18:34:10 +0700

    add more video data

--- src/constant/data/video.ts
+++ src/constant/data/video.ts
@@ -8,119 +8,119 @@ export interface VideoData {
 
 export const videoUrls: VideoData[] = [
   {
-    title: "TULUS - Hati-Hati di Jalan (Official Lyric Video)",
+    title: "TULUS - Hati-Hati di Jalan",
     thumbnail:
       "https://i.ytimg.com/vi/_N6vSc_mT6I/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDNnjDgSWIKHltC_WHkW6ZsNSLKwQ",
     link: "https://www.youtube.com/watch?v=_N6vSc_mT6I",
     artist: "Tulus",
   },
   {
-    title: "TULUS - Monokrom (Official Music Video)",
+    title: "TULUS - Monokrom",
     thumbnail:
       "https://i.ytimg.com/vi/QqJ-Vp8mvbk/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAXMC7DktlvXdktxePlhMCIHSmFIg",
     link: "https://www.youtube.com/watch?v=QqJ-Vp8mvbk",
     artist: "Tulus",
   },
   {
-    title: "TULUS - Pamit (Official Music Video)",
+    title: "TULUS - Pamit",
     thumbnail:
       "https://i.ytimg.com/vi/G2-ZC9NpDBg/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBS9D6GiYPH-lDWgrKLok6-bYYXKg",
     link: "https://www.youtube.com/watch?v=G2-ZC9NpDBg",
     artist: "Tulus",
   },
   {
-    title: "TULUS - Ruang Sendiri (Official Music Video)",
+    title: "TULUS - Ruang Sendiri",
     thumbnail:
       "https://i.ytimg.com/vi/c0p-61mLUGw/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAJxcgbB5_9Zyj4MYV26zIiSloYhw",
     link: "https://www.youtube.com/watch?v=c0p-61mLUGw",
     artist: "Tulus",
   },
   {
-    title: "TULUS - Interaksi (Official Lyric Video)",
+    title: "TULUS - Interaksi",
     thumbnail:
       "https://i.ytimg.com/vi/GIy9ZbH0sHo/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA6cVk5_Zk1KlivWtK4SfU8DjUUKQ",
     link: "https://www.youtube.com/watch?v=GIy9ZbH0sHo",
     artist: "Tulus",
   },
   {
-    title: "TULUS - Teman Hidup (Official Music Video)",
+    title: "TULUS - Teman Hidup",
     thumbnail:
       "https://i.ytimg.com/vi/dt4Ueda_h6Y/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDqxDcgJ_BxIsHhEVfJdeoHPtkoRw",
     link: "https://www.youtube.com/watch?v=dt4Ueda_h6Y",
     artist: "Tulus",
   },
   {
-    title: "TULUS - Jangan Cintai Aku Apa Adanya (Official Music Video)",
+    title: "TULUS - Jangan Cintai Aku Apa Adanya",
     thumbnail:
       "https://i.ytimg.com/vi/5L1RVCtL1D0/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDwE9oD_hGIPK3W8L8HohPXJh8CzQ",
     link: "https://www.youtube.com/watch?v=5L1RVCtL1D0",
     artist: "Tulus",
   },
   {
-    title: "TULUS - Diri (Official Lyric Video)",
+    title: "TULUS - Diri",
     thumbnail:
       "https://i.ytimg.com/vi/fsGcUWiylW8/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCp6DAnjH-Mh9M3lqq_ilD2zgMThg",
     link: "https://www.youtube.com/watch?v=fsGcUWiylW8",
     artist: "Tulus",
   },
   {
-    title: "TULUS - Sepatu (Official Music Video)",
+    title: "TULUS - Sepatu",
     thumbnail:
       "https://i.ytimg.com/vi/DYbSp-B-8AM/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCaNnyrbThNO3ylDxe3uV4-_RaKnw",
     link: "https://www.youtube.com/watch?v=DYbSp-B-8AM",
     artist: "Tulus",
   },
   {
-    title: "TULUS - Sewindu (Official Music Video)",
+    title: "TULUS - Sewindu",
     thumbnail:
       "https://i.ytimg.com/vi/wpst_4m_c-E/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLChnBLzO6KKWEO2K0gJeK3Px9FriQ",
     link: "https://www.youtube.com/watch?v=wpst_4m_c-E",
     artist: "Tulus",
   },
   {
-    title: "TULUS - Gajah (Official Music Video)",
+    title: "TULUS - Gajah",
     thumbnail:
       "https://i.ytimg.com/vi/I-el8UadDc4/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCCkttVp9DKzFpj3p_icvemmxui7g",
     link: "https://www.youtube.com/watch?v=I-el8UadDc4",
     artist: "Tulus",
   },
   {
-    title: "TULUS - Jatuh Suka (Official Lyric Video)",
+    title: "TULUS - Jatuh Suka",
     thumbnail:
       "https://i.ytimg.com/vi/2uJut6USftQ/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBtagM3bfoswKbO81TBopsadB1oUA",
     link: "https://www.youtube.com/watch?v=2uJut6USftQ",
     artist: "Tulus",
   },
   {
-    title: "TULUS - Tukar Jiwa (Official Music Video)",
+    title: "TULUS - Tukar Jiwa",
     thumbnail:
       "https://i.ytimg.com/vi/aHPUWgYgees/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA2j89IFkBSA70sGH091nTBOIYiLw",
     link: "https://www.youtube.com/watch?v=aHPUWgYgees",
     artist: "Tulus",
   },
   {
-    title: "TULUS - Langit Abu-abu (Official Lyric Video)",
+    title: "TULUS - Langit Abu-abu",
     thumbnail:
       "https://i.ytimg.com/vi/CVI6SOuPAAw/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDZx2P0EaafqdJeMRjhu345aEAwQg",
     link: "https://www.youtube.com/watch?v=CVI6SOuPAAw",
     artist: "Tulus",
   },
   {
-    title: "TULUS - Labirin (Official Music Video)",
+    title: "TULUS - Labirin",
     thumbnail:
       "https://i.ytimg.com/vi/ANLIBOKQVLc/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC89ZNVv64qZd9B5GEidMX57IKK5w",
     link: "https://www.youtube.com/watch?v=ANLIBOKQVLc",
     artist: "Tulus",
   },
   {
-    title: "TULUS - Cahaya (Official Lyric Video)",
+    title: "TULUS - Cahaya",
     thumbnail:
       "https://i.ytimg.com/vi/-BgPEiLABtQ/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBZkpl3Y-hp6sd_wo7-AEEs7iekjQ",
     link: "https://www.youtube.com/watch?v=-BgPEiLABtQ",
     artist: "Tulus",
   },
   {
-    title: "Yovie Tulus Glenn - Adu Rayu (OFFICIAL LYRIC VIDEO)",
+    title: "Yovie Tulus Glenn - Adu Rayu",
     thumbnail:
       "https://i.ytimg.com/vi/rfWRkrhN2bA/hq720.jpg?sqp=-oaymwE2COgCEMoBSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB_gmAAtAFigIMCAAQARhiIGIoYjAP&rs=AOn4CLAFH9RqGU86S9jclBA5G-8KspiCRg",
     link: "https://www.youtube.com/watch?v=rfWRkrhN2bA",
@@ -141,24 +141,234 @@ export const videoUrls: VideoData[] = [
     artist: "TheOvertunes",
   },
   {
-    title: "Juicy Luicy - Lantas (Official Lyric Video)",
+    title: "Juicy Luicy - Lantas",
     thumbnail:
       "https://i.ytimg.com/vi/ba-XAIskH_g/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD4JMZ-9sveU5ALpeEAdBdJ9kewnA",
     link: "https://www.youtube.com/watch?v=ba-XAIskH_g",
     artist: "Juicy Luicy",
   },
   {
-    title: "Juicy Luicy - Tampar (Official Lyric Video)",
+    title: "Edcoustic - Muhasabah Cinta",
     thumbnail:
-      "https://i.ytimg.com/vi/qmgA_WejI8w/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDB4Oa7CtiVWL6G1W9bwaNjicK-2w",
-    link: "https://www.youtube.com/watch?v=qmgA_WejI8w",
-    artist: "Juicy Luicy",
+      "https://i.ytimg.com/vi/BwR9oFngvXE/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCZisRNmjpadj_1it4ET9auErB65g",
+    link: "https://www.youtube.com/watch?v=BwR9oFngvXE",
+    artist: "Edcoustic",
   },
   {
-    title: "Juicy Luicy - Tanpa Tergesa (Official Music Video)",
+    title: "Inteam feat. Edcoustic - Kau Ditakdirkan Untukku",
     thumbnail:
-      "https://i.ytimg.com/vi/tUUW5QAdAKk/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB5v7SydQtjv8AQRQjLulmEKjH4BA",
-    link: "https://www.youtube.com/watch?v=tUUW5QAdAKk",
-    artist: "Juicy Luicy",
+      "https://i.ytimg.com/vi/BwR9oFngvXE/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCZisRNmjpadj_1it4ET9auErB65g",
+    link: "https://www.youtube.com/watch?v=BwR9oFngvXE",
+    artist: "Edcoustic",
+  },
+  {
+    title: "EdCoustic - Nantikan aku di batas waktu",
+    thumbnail:
+      "https://i.ytimg.com/vi/INRTiMFlBTI/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB8QwBfaQWq4ejywQ_ohFTGC1lpoA",
+    link: "https://www.youtube.com/watch?v=INRTiMFlBTI",
+    artist: "Edcoustic",
+  },
+  {
+    title: "Maher Zain - Ya Nabi Salam Alayka",
+    thumbnail:
+      "https://i.ytimg.com/vi/Vqfy4ScRXFQ/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBfFvpsDvNCShA8kWF-bthmVP0hSQ",
+    link: "https://www.youtube.com/watch?v=Vqfy4ScRXFQ",
+    artist: "Maher Zain",
+  },
+  {
+    title: "Maher Zain - Mawlaya (Arabic)",
+    thumbnail:
+      "https://i.ytimg.com/vi/uqaKaUVV4v0/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBnNia7z_99aO_HM5IdXAAy0ScLvg",
+    link: "https://www.youtube.com/watch?v=uqaKaUVV4v0",
+    artist: "Maher Zain",
+  },
+  {
+    title: "Maher Zain - Assalamu Alayka (Arabic)",
+    thumbnail:
+      "https://i.ytimg.com/vi/xwg9Lf5ruUE/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBUTJxZTkD7GAe_p1lmv_i2T4iuAg",
+    link: "https://www.youtube.com/watch?v=xwg9Lf5ruUE",
+    artist: "Maher Zain",
+  },
+  {
+    title: "Maher Zain - Number One For Me",
+    thumbnail:
+      "https://i.ytimg.com/vi/f4J7-WR1QOY/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBUCjVWwCw6R_iSQ0woTdoAAbWLWA",
+    link: "https://www.youtube.com/watch?v=f4J7-WR1QOY",
+    artist: "Maher Zain",
+  },
+  {
+    title: "Maher Zain - Baraka Allahu Lakuma",
+    thumbnail:
+      "https://i.ytimg.com/vi/mHpTdsBbYRM/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCK46tfhZSfTCVNE3YOkfkJjOokOg",
+    link: "https://www.youtube.com/watch?v=mHpTdsBbYRM",
+    artist: "Maher Zain",
+  },
+  {
+    title: "Maher Zain - Radhitu Billahi (Arabic)",
+    thumbnail:
+      "https://i.ytimg.com/vi/AjzE4Uj7kIM/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC1c1oZJvcNhQTyNDrGiznYxb8gNw",
+    link: "https://www.youtube.com/watch?v=AjzE4Uj7kIM",
+    artist: "Maher Zain",
+  },
+  {
+    title: "Maher Zain - Insya Allah",
+    thumbnail:
+      "https://i.ytimg.com/vi/KfXIF2Mm2Kc/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBXGGBQDfDsxsNaPZUuofqyhBOV9Q",
+    link: "https://www.youtube.com/watch?v=KfXIF2Mm2Kc",
+    artist: "Maher Zain",
+  },
+  {
+    title: "Maher Zain - Ramadan (Arabic)",
+    thumbnail:
+      "https://i.ytimg.com/vi/jnJdXj3P26M/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAk_7ihPo9MSDtLAcPcwg0FkrkKew",
+    link: "https://www.youtube.com/watch?v=jnJdXj3P26M",
+    artist: "Maher Zain",
+  },
+  {
+    title: "Maher Zain - Thank You Allah",
+    thumbnail:
+      "https://i.ytimg.com/vi/RBrdl0v_anc/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAean-jG9Tm3iwN977-f1AyMd2sVQ",
+    link: "https://www.youtube.com/watch?v=RBrdl0v_anc",
+    artist: "Maher Zain",
+  },
+  {
+    title: "Maher Zain - Assubhu Bada",
+    thumbnail:
+      "https://i.ytimg.com/vi/ouKANRi0Fyo/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBooBXEXPdazppEUNMYHV1gbTRkQQ",
+    link: "https://www.youtube.com/watch?v=ouKANRi0Fyo",
+    artist: "Maher Zain",
+  },
+  {
+    title: "Maher Zain - For The Rest Of My Life",
+    thumbnail:
+      "https://i.ytimg.com/vi/PHbZ9SXHJwA/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBzw3fyoyzZxtUSyAUimGuXALJUzw",
+    link: "https://www.youtube.com/watch?v=PHbZ9SXHJwA",
+    artist: "Maher Zain",
+  },
+  {
+    title: "Maher Zain - Muhammad (Pbuh) [Waheshna]",
+    thumbnail:
+      "https://i.ytimg.com/vi/BiGVVAP3KsQ/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBj_gVyC7X4eRc0UMePGC8ZxDhbjw",
+    link: "https://www.youtube.com/watch?v=BiGVVAP3KsQ",
+    artist: "Maher Zain",
+  },
+  {
+    title: "あいみょん - マリーゴールド",
+    thumbnail:
+      "https://i.ytimg.com/vi/0xSiBpUdW4E/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDHKziYhEOd7H2XvtlbXtnM1Af7kw",
+    link: "https://www.youtube.com/watch?v=0xSiBpUdW4E",
+    artist: "Aimyon (あいみょん)",
+  },
+  {
+    title: "あいみょん - 裸の心",
+    thumbnail:
+      "https://i.ytimg.com/vi/yOAwvRmVIyo/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAwQNLSPv3WniOnr_WLhLRvdwRibg",
+    link: "https://www.youtube.com/watch?v=yOAwvRmVIyo",
+    artist: "Aimyon (あいみょん)",
+  },
+  {
+    title: "あいみょん – ハルノヒ",
+    thumbnail:
+      "https://i.ytimg.com/vi/pfGI91CFtRg/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBN6USIyAc3XPOXEqRRdflMX2KAew",
+    link: "https://www.youtube.com/watch?v=pfGI91CFtRg",
+    artist: "Aimyon (あいみょん)",
+  },
+  {
+    title: " あいみょん - 愛を伝えたいだとか ",
+    thumbnail:
+      "https://i.ytimg.com/vi/9qRCARM_LfE/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDZrl6p1hmkop4s8U69gU1VVznK6A",
+    link: "https://www.youtube.com/watch?v=9qRCARM_LfE",
+    artist: "Aimyon (あいみょん)",
+  },
+  {
+    title: "あいみょん - 君はロックを聴かない ",
+    thumbnail:
+      "https://i.ytimg.com/vi/ARwVe1MYAUA/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBOYLwLTQSk-lJNmTAUa7cNCXQYlQ",
+    link: "https://www.youtube.com/watch?v=ARwVe1MYAUA",
+    artist: "Aimyon (あいみょん)",
+  },
+  {
+    title: "あいみょん - 今夜このまま",
+    thumbnail:
+      "https://i.ytimg.com/vi/mH6LoI63buY/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDA46-8Vm1Rclwv_lNdW1rvb4mVPw",
+    link: "https://www.youtube.com/watch?v=mH6LoI63buY",
+    artist: "Aimyon (あいみょん)",
+  },
+  {
+    title: "あいみょん – 空の青さを知る人よ",
+    thumbnail:
+      "https://i.ytimg.com/vi/ztdpBUDf00o/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCOjmvthhen7-MMJZhusWZuJrAsRQ",
+    link: "https://www.youtube.com/watch?v=ztdpBUDf00o",
+    artist: "Aimyon (あいみょん)",
+  },
+  {
+    title: "優里『ドライフラワー』 -ディレクターズカットver.-",
+    thumbnail:
+      "https://i.ytimg.com/vi/kzZ6KXDM1RI/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDcO5kWDXo-Okt0q_dr8Y3te12X1g",
+    link: "https://www.youtube.com/watch?v=kzZ6KXDM1RI",
+    artist: "Yuuri",
+  },
+  {
+    title: "優里『ベテルギウス』",
+    thumbnail:
+      "https://i.ytimg.com/vi/cbqvxDTLMps/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLANJ2UIgBj_A2xoG2LlETLIVbbV5w",
+    link: "https://www.youtube.com/watch?v=cbqvxDTLMps",
+    artist: "Yuuri",
+  },
+  {
+    title: "優里『ベテルギウス』",
+    thumbnail:
+      "https://i.ytimg.com/vi/uxYLXaXtH9I/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAImo8kfvtrQktcFBg9aoSAbBhpjg",
+    link: "https://www.youtube.com/watch?v=cbqvxDTLMps",
+    artist: "Yuuri",
+  },
+  {
+    title: "Yoroshika - Just Sunny To You",
+    thumbnail:
+      "https://i.ytimg.com/vi/-VKIqrvVOpo/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDF4kInv14afxMx_3JRGHstO2xSzw",
+    link: "https://www.youtube.com/watch?v=-VKIqrvVOpo",
+    artist: "Yorushika",
+  },
+  {
+    title: "Yorushika - だから僕は音楽を辞めた",
+    thumbnail:
+      "https://i.ytimg.com/vi/KTZ-y85Erus/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCTW1WUSTf28uFHkpRqeSBWsbr6eg",
+    link: "https://www.youtube.com/watch?v=KTZ-y85Erus",
+    artist: "Yorushika",
+  },
+  {
+    title: "優里『ベテルギウス』",
+    thumbnail:
+      "https://i.ytimg.com/vi/F64yFFnZfkI/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLATgpr2HKwmt4pq5CuBdkRBDcoGxw",
+    link: "https://www.youtube.com/watch?v=F64yFFnZfkI",
+    artist: "Yorushika",
+  },
+  {
+    title: "Yorushika - itte",
+    thumbnail:
+      "https://i.ytimg.com/vi/-VKIqrvVOpo/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDF4kInv14afxMx_3JRGHstO2xSzw",
+    link: "https://www.youtube.com/watch?v=-VKIqrvVOpo",
+    artist: "Yorushika",
+  },
+  {
+    title: "Yorushika - The Ghost of Flowers",
+    thumbnail:
+      "https://i.ytimg.com/vi/9lVPAWLWtWc/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD-QkOwG1DHNlBpq15xxDm1weFuDg",
+    link: "https://www.youtube.com/watch?v=9lVPAWLWtWc",
+    artist: "Yorushika",
+  },
+  {
+    title: "ヨルシカ - 春泥棒",
+    thumbnail:
+      "https://i.ytimg.com/vi/Sw1Flgub9s8/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC643_JLgN1iUTzxwZmRtwt2bI5nw",
+    link: "https://www.youtube.com/watch?v=Sw1Flgub9s8",
+    artist: "Yorushika",
+  },
+  {
+    title: "Yorushika - Hitchcock",
+    thumbnail:
+      "https://i.ytimg.com/vi/t7MBzMP4OzY/hqdefault.jpg?sqp=-oaymwE2CNACELwBSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB_gmAAtAFigIMCAAQARhlIGUoZTAP&rs=AOn4CLClZMjVwXKAM6725lpyndO_jydKww",
+    link: "https://www.youtube.com/watch?v=t7MBzMP4OzY",
+    artist: "Yorushika",
   },
 ];
