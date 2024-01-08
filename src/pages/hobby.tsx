8278ff806eaaf0c91ed242030baddaff41dd2f5b
RizkyFauziIlmi <rizkyfauziilmi@rizkyfauziilmi.com>
2024-01-08 10:43:17 +0700

    add confirmation dialog for music tab

--- src/pages/hobby.tsx
+++ src/pages/hobby.tsx
@@ -1,22 +1,55 @@
 import { GameHobbyContent } from "@/components/content/game-hobby-content";
 import { MusicHobbyContent } from "@/components/content/music-hobby-content";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { Gamepad2, Headphones } from "lucide-react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Gamepad2, Headphones, Speaker, Volume2 } from "lucide-react";
 import { useDocumentTitle } from "usehooks-ts";
+import { useState } from "react";
 
 export default function Hobby() {
   useDocumentTitle("Rizky Fauzi Ilmi - Hobby");
+  const [tabState, setTabState] = useState("game");
 
   return (
     <div className="w-screen md:w-full h-full overflow-y-auto">
-      <Tabs defaultValue="game" className="w-full h-full">
+      <Tabs value={tabState} className="w-full h-full">
         <TabsList className="mx-5">
-          <TabsTrigger value="game">
+          <TabsTrigger value="game" onClick={() => setTabState("game")}>
             <Gamepad2 className="h-4 w-4 mr-2" /> Game
           </TabsTrigger>
-          <TabsTrigger value="music">
-            <Headphones className="h-4 w-4 mr-2" /> Music
-          </TabsTrigger>
+          <AlertDialog>
+            <AlertDialogTrigger>
+              <TabsTrigger value="music">
+                <Headphones className="h-4 w-4 mr-2" /> Music
+              </TabsTrigger>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle className="flex items-center gap-2">
+                  <Volume2 /> Audio Playback Confirmation
+                </AlertDialogTitle>
+                <AlertDialogDescription>
+                  This action will play audio in background, are you sure?
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel>No, Cancel</AlertDialogCancel>
+                <AlertDialogAction onClick={() => setTabState("music")}>
+                  <Speaker className="w-4 h-4 mr-1" /> Yes, Play
+                </AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
         </TabsList>
         <TabsContent value="game">
           <GameHobbyContent />
